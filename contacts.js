c36bd12d332953c79764ba493e0b26b151010d41
Oleksandr220 <74794729+Oleksandr220@users.noreply.github.com>
2021-11-27 20:32:59 +0200

    summary

--- contacts.js
+++ contacts.js
@@ -1,49 +1,49 @@
-const fs = require("fs/promises");
-const path = require("path");
+const fs = require('fs/promises')
+const path = require('path')
+const { nanoid } = require('nanoid')
 
-const contactsPath = path.join(__dirname, "db/contacts.json");
+const contactsPath = path.join(__dirname, 'db/contacts.json')
 
 const updateContacts = async (newContact) => {
-  const contactsString = JSON.stringify(newContact);
-  await fs.writeFile(contactsPath, contactsString);
-};
+  const contactsString = JSON.stringify(newContact)
+  await fs.writeFile(contactsPath, contactsString)
+}
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
-  return contacts;
+  const data = await fs.readFile(contactsPath, utf8)
+  const contacts = JSON.parse(data)
+  return contacts
 }
 
 async function getContactById(contactId) {
-  const contacts = await listContacts(contactsPath);
-  const contactById = contacts.find((contact) => contact.id === contactId);
+  const contacts = await listContacts(contactsPath)
+  const contactById = contacts.find((contact) => contact.id === contactId)
   if (!contactById) {
-    return null;
+    return null
   }
-  return contactById;
+  return contactById
 }
 
 async function removeContact(contactId) {
-  const contacts = await listContacts(contactsPath);
-  const idx = contacts.findIndex((contact) => contact.id === contactId);
+  const contacts = await listContacts(contactsPath)
+  const idx = contacts.findIndex((contact) => contact.id === contactId)
   if (idx === -1) {
-    return null;
+    return null
   }
-  const removeContact = contacts.splice(idx, 1);
-  await updateContacts(contacts);
-  return removeContact;
+  const removeContact = contacts.splice(idx, 1)
+  await updateContacts(contacts)
+  return removeContact
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await listContacts(contactsPath);
-  const lastContactOfArray = contacts[contacts.length - 1];
-  const id = lastContactOfArray.id + 1;
-  const newContact = { name, email, phone, id };
-  contacts.push(newContact);
-  await updateContacts(contacts);
-  return newContact;
+  const contacts = await listContacts(contactsPath)
+  const id = nanoid
+  const newContact = { name, email, phone, id }
+  contacts.push(newContact)
+  await updateContacts(contacts)
+  return newContact
 }
 
-addContact();
+addContact()
 
-module.exports = { listContacts, getContactById, removeContact, addContact };
+module.exports = { listContacts, getContactById, removeContact, addContact }
